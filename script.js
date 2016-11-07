a7608dfc57a2568b804da5383e00738f96f784bb
sophiayangx <sopyang@nuevaschool.org>
2016-11-07 08:35:46 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,15 @@
+// demo function
+function logArgs(args) {
+  var a = 1
+  console.log(args)
+}
+
+var functionString = "how do I get the function as a string?"
+
+var consoleLogExample = $("how do I get the element where I want to put the example?")
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(contents)
+
+consoleLogExample.html(formattedExample)
