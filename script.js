943b8a151567517769123d85ae017b7bfc530e99
sophiayangx <sopyang@nuevaschool.org>
2016-11-08 22:57:55 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -39,6 +39,22 @@ var button = $("#button").click(function() {
   console.log("Jquery Example");
 });
 
+$(document).ready(function() {
+  $('div').mouseenter(function() {
+    $(this).animate({
+      height: '+=10px'
+    });
+  });
+  $('div').mouseleave(function() {
+    $(this).animate({
+      height: '-=10px'
+    }); 
+  });
+  $('div').click(function() {
+    $(this).toggle(1000);
+  }); 
+});
+
 var functionString = button.toString()
 
 var buttonExample = $("#button")
