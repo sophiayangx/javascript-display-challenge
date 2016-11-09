7b0810119a3899be5789e2456dbfad4c50259476
sophiayangx <sopyang@nuevaschool.org>
2016-11-09 09:46:38 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -31,21 +31,11 @@ formattedExample.text(functionString)
 functionExample.html(formattedExample)
 
 //function takes in function
-var $("#function-input").keydown(function(e) {	
+$("#function-input").keydown(function(e) {	
   if (e.keyCode == "13") {
 console.log("hello")
   }
 })
-
-var functionString = perimeter.toString() + call
-
-var functionExample = $("#function-example")
-
-var formattedExample = $("<pre>")
-
-formattedExample.text(functionString)
-
-functionExample.html(formattedExample)
 
 //jquery
 $("#button").click(function() {
