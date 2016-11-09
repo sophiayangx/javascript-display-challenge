1f149ccf11dc1225c44d8712f1c6e844a5fc2b51
sophiayangx <sopyang@nuevaschool.org>
2016-11-09 09:36:31 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -32,12 +32,12 @@ functionExample.html(formattedExample)
 
 //function takes in function
 
-input.keydown(function(e) {	
-  if (e.keyCode == "13") {
-    var valueToSave = input.val();
-    addListItem("sophiasList", valueToSave);
-  }
-})
+//keydown(function(e) {	
+//  if (e.keyCode == "13") {
+//    var valueToSave = input.val();
+//    addListItem("sophiasList", valueToSave);
+//  }
+//})
 
 //jquery
 $("#button").click(function() {
